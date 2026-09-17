5d80f65a97924e5b221b1e9168f8f74118dd4ba8
Aditya Raj <154418353+Codeadityaa@users.noreply.github.com>
2026-09-17 14:32:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let timerDisplay=document.getElementById("timer");
+let currentDate=new Date();
+set interval(
+	function () {
+		currentDate=new Date();
+		timerDisplay.innerHTML=currentDate.tolocaleString();
+	},1000
+		
+	}
+)
